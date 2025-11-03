f1ec78a814278a0768a1db64fd55a3d9d6167456
krwong <krwong@email.unc.edu>
2025-11-03 10:10:03 -0500

    add jackson to pom.xml, add AVInfoService to retrieve av info wiht ffprobe and use encode/copy depending on ffprobe output, add tests and test resource

---
+++ src/main/java/mp44u/services/AVInfoService.java
@@ -0,0 +1,103 @@
+package mp44u.services;
+
+import mp44u.options.Mp44uOptions;
+import mp44u.util.CommandUtility;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Service for retrieving audio/video information using ffprobe
+ * @author krwong
+ */
+public class AVInfoService {
+    private static final Logger log = getLogger(AVInfoService.class);
+
+    /**
+     * Run ffprobe and retrieve AV information for the input file
+     * @param options
+     * @return
+      */
+    public Map<String,String> retrieveAudioVideoInfo(Mp44uOptions options) throws Exception {
+        // run ffprobe
+        String inputFile = options.getInputPath().toString();
+        String ffprobe = "ffprobe";
+        String v = "-v";
+        String quiet = "quiet";
+        String showEntries = "-show_entries";
+        String entries = "stream=codec_name,height,codec_type,bit_rate";
+
+        List<String> command = new ArrayList<>(Arrays.asList(ffprobe, v, quiet,
+                showEntries, entries, inputFile));
+        String ffprobeOutput = CommandUtility.executeCommand(command);
+
+        // split ffprobe output around \n and filter for values containing =
+        Map<String,String> avInfo = new HashMap<>();
+        List<String> streams = List.of(ffprobeOutput.split(System.lineSeparator()));
+        List<String> streamInfo = streams.stream()
+                .filter(str -> str.matches("[a-zA-z]*=[a-zA-z]*[0-9]*")).limit(4).toList();
+
+        for (String stream : streamInfo) {
+            avInfo.put(stream.split("=")[0], stream.split("=")[1]);
+        }
+
+        return avInfo;
+    }
+
+    /**
+     * Use AUDIO_ENCODE if false and AUDIO_COPY if true for all audio streams in file
+     * codec_name: aac AND bit_rate <= 192000 (i.e., 192kbps)
+     * @param options
+     * @return
+     */
+    public String audioEncodeOrCopy(Mp44uOptions options) throws Exception {
+        String audioEncoding = "AUDIO_ENCODE";
+
+        Map<String,String> avInfo = retrieveAudioVideoInfo(options);
+        String codecName = avInfo.get("codec_name");
+        int bitRate;
+        if (avInfo.get("bit_rate") != null) {
+            bitRate = Integer.parseInt(avInfo.get("bit_rate"));
+        } else {
+            bitRate = 192000;
+        }
+
+        if (codecName.contains("aac") && bitRate <= 192000) {
+            audioEncoding = "AUDIO_COPY";
+        }
+
+        return audioEncoding;
+    }
+
+    /**
+     * Use VIDEO_ENCODE if false and VIDEO_COPY if true for all video streams in file
+     * codec_name: h264 AND height: <=720 AND bit_rate <= 3000000 (i.e., 3Mbps)
+     * @param options
+     * @return
+     */
+    public String videoEncodeOrCopy(Mp44uOptions options) throws Exception {
+        String videoEncoding = "VIDEO_ENCODE";
+
+        Map<String,String> avInfo = retrieveAudioVideoInfo(options);
+        String codecName = avInfo.get("codec_name");
+        int height = Integer.parseInt(avInfo.get("height"));
+        int bitRate;
+        if (avInfo.get("bit_rate") != null) {
+            bitRate = Integer.parseInt(avInfo.get("bit_rate"));
+        } else {
+            bitRate = 3000000;
+        }
+
+        if (codecName.contains("h264") && height <= 720 && bitRate <= 3000000) {
+            videoEncoding = "VIDEO_COPY";
+        }
+
+        return videoEncoding;
+    }
+}
---
+++ src/test/java/mp44u/services/AVInfoServiceTest.java
@@ -0,0 +1,220 @@
+package mp44u.services;
+
+import mp44u.options.Mp44uOptions;
+import mp44u.util.CommandUtility;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+public class AVInfoServiceTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private AutoCloseable closeable;
+    private AVInfoService service;
+    private Path testOutput = Paths.get("target/test_output");
+
+    @BeforeEach
+    public void setup() throws Exception {
+        closeable = openMocks(this);
+        System.setOut(new PrintStream(outputStreamCaptor));
+        service = new AVInfoService();
+
+        if (Files.notExists(testOutput)) {
+            Files.createDirectory(testOutput);
+        }
+    }
+
+    @AfterEach
+    public void close() throws Exception {
+        FileUtils.deleteDirectory(testOutput.toFile());
+        closeable.close();
+    }
+
+    @Test
+    public void testRetrieveAudioInfo() throws Exception {
+        try (MockedStatic<CommandUtility> mockedStatic = Mockito.mockStatic(CommandUtility.class)) {
+            Path mockedInput = testOutput.resolve("test_input.mp3");
+            Mp44uOptions options = new Mp44uOptions();
+            options.setInputPath(mockedInput);
+            mockedStatic.when(() -> CommandUtility.executeCommand(anyList())).thenReturn("{\n" +
+                    "[STREAM]\n" +
+                    "codec_name=mp3\n" +
+                    "codec_type=audio\n" +
+                    "bit_rate=128000\n" +
+                    "[/STREAM]");
+
+            AVInfoService avInfoService = new AVInfoService();
+            Map<String,String> testInfo = avInfoService.retrieveAudioVideoInfo(options);
+
+            mockedStatic.verify(() -> CommandUtility.executeCommand(
+                    new ArrayList<>(Arrays.asList("ffprobe", "-v", "quiet",
+                            "-show_entries", "stream=codec_name,height,codec_type,bit_rate", mockedInput.toString()))));
+            assertTrue(testInfo.containsKey("codec_name"));
+            assertTrue(testInfo.containsValue("mp3"));
+            assertTrue(testInfo.containsKey("bit_rate"));
+            assertTrue(testInfo.containsValue("128000"));
+        }
+    }
+
+    @Test
+    public void testRetrieveVideoInfo() throws Exception {
+        try (MockedStatic<CommandUtility> mockedStatic = Mockito.mockStatic(CommandUtility.class)) {
+            Path mockedInput = testOutput.resolve("test_input.mp4");
+            Mp44uOptions options = new Mp44uOptions();
+            options.setInputPath(mockedInput);
+            mockedStatic.when(() -> CommandUtility.executeCommand(anyList())).thenReturn("{\n" +
+                    "[STREAM]\n" +
+                    "codec_name=h264\n" +
+                    "codec_type=video\n" +
+                    "height=480\n" +
+                    "bit_rate=923373\n" +
+                    "[/STREAM]\n");
+
+            AVInfoService avInfoService = new AVInfoService();
+            Map<String,String> testInfo = avInfoService.retrieveAudioVideoInfo(options);
+
+            mockedStatic.verify(() -> CommandUtility.executeCommand(
+                    new ArrayList<>(Arrays.asList("ffprobe", "-v", "quiet",
+                            "-show_entries", "stream=codec_name,height,codec_type,bit_rate", mockedInput.toString()))));
+            assertTrue(testInfo.containsKey("codec_name"));
+            assertTrue(testInfo.containsValue("h264"));
+            assertTrue(testInfo.containsKey("height"));
+            assertTrue(testInfo.containsValue("480"));
+            assertTrue(testInfo.containsKey("bit_rate"));
+            assertTrue(testInfo.containsValue("923373"));
+        }
+    }
+
+    @Test
+    public void testAudioEncodeOrCopy() throws Exception {
+        try (MockedStatic<CommandUtility> mockedStatic = Mockito.mockStatic(CommandUtility.class)) {
+            Path mockedInput = testOutput.resolve("test_input.m4a");
+            Mp44uOptions options = new Mp44uOptions();
+            options.setInputPath(mockedInput);
+            mockedStatic.when(() -> CommandUtility.executeCommand(anyList())).thenReturn("{\n" +
+                    "[STREAM]\n" +
+                    "codec_name=aac\n" +
+                    "codec_type=audio\n" +
+                    "bit_rate=128000\n" +
+                    "[/STREAM]");
+
+            AVInfoService avInfoService = new AVInfoService();
+            String encodeOrCopy = avInfoService.audioEncodeOrCopy(options);
+
+            mockedStatic.verify(() -> CommandUtility.executeCommand(
+                    new ArrayList<>(Arrays.asList("ffprobe", "-v", "quiet",
+                            "-show_entries", "stream=codec_name,height,codec_type,bit_rate", mockedInput.toString()))));
+            assertEquals("AUDIO_COPY", encodeOrCopy);
+        }
+    }
+
+    @Test
+    public void testVideoEncodeOrCopy() throws Exception {
+        try (MockedStatic<CommandUtility> mockedStatic = Mockito.mockStatic(CommandUtility.class)) {
+            Path mockedInput = testOutput.resolve("test_input.mp4");
+            Mp44uOptions options = new Mp44uOptions();
+            options.setInputPath(mockedInput);
+            mockedStatic.when(() -> CommandUtility.executeCommand(anyList())).thenReturn("{\n" +
+                    "[STREAM]\n" +
+                    "codec_name=h264\n" +
+                    "codec_type=video\n" +
+                    "height=480\n" +
+                    "bit_rate=923373\n" +
+                    "[/STREAM]\n");
+
+            AVInfoService avInfoService = new AVInfoService();
+            String encodeOrCopy = avInfoService.videoEncodeOrCopy(options);
+
+            mockedStatic.verify(() -> CommandUtility.executeCommand(
+                    new ArrayList<>(Arrays.asList("ffprobe", "-v", "quiet",
+                            "-show_entries", "stream=codec_name,height,codec_type,bit_rate", mockedInput.toString()))));
+            assertEquals("VIDEO_COPY", encodeOrCopy);
+        }
+    }
+
+    @Test
+    public void testRetrieveAudioAac() throws Exception {
+        Path testFile = Path.of("src/test/resources/04007_G0010_2_2.aac");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        Map<String,String> testInfo = service.retrieveAudioVideoInfo(options);
+        assertTrue(testInfo.containsKey("codec_name"));
+        assertTrue(testInfo.containsValue("aac"));
+        assertTrue(testInfo.containsKey("bit_rate"));
+        assertTrue(testInfo.containsValue("128000"));
+    }
+
+    @Test
+    public void testRetrieveVideoMp4() throws Exception {
+        Path testFile = Path.of("src/test/resources/009.mp4");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        service.retrieveAudioVideoInfo(options);
+
+        Map<String,String> testInfo = service.retrieveAudioVideoInfo(options);
+        assertTrue(testInfo.containsKey("codec_name"));
+        assertTrue(testInfo.containsValue("h264"));
+        assertTrue(testInfo.containsKey("height"));
+        assertTrue(testInfo.containsValue("480"));
+        assertTrue(testInfo.containsKey("bit_rate"));
+        assertTrue(testInfo.containsValue("923373"));
+    }
+
+    @Test
+    public void testAudioEncodeOrCopyAac() throws Exception {
+        Path testFile = Path.of("src/test/resources/04007_G0010_2_2.m4a");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        String encodeOrCopy = service.audioEncodeOrCopy(options);
+        assertEquals("AUDIO_COPY", encodeOrCopy);
+    }
+
+    @Test
+    public void testAudioEncodeOrCopyMov() throws Exception {
+        Path testFile = Path.of("src/test/resources/AMEN.MOV");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        String encodeOrCopy = service.audioEncodeOrCopy(options);
+        assertEquals("AUDIO_ENCODE", encodeOrCopy);
+    }
+
+    @Test
+    public void testVideoEncodeOrCopyMp4() throws Exception {
+        Path testFile = Path.of("src/test/resources/009.mp4");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        String encodeOrCopy = service.videoEncodeOrCopy(options);
+        assertEquals("VIDEO_COPY", encodeOrCopy);
+    }
+
+    @Test
+    public void testVideoEncodeOrCopyMov() throws Exception {
+        Path testFile = Path.of("src/test/resources/00288.MTS");
+        Mp44uOptions options = new Mp44uOptions();
+        options.setInputPath(testFile);
+
+        String encodeOrCopy = service.videoEncodeOrCopy(options);
+        assertEquals("VIDEO_ENCODE", encodeOrCopy);
+    }
+}
