d3bff3a7a79ce3141d277e0e1285dd7fd98deb07
Ben Pennell <bbpennel@email.unc.edu>
2026-03-09 11:01:57 -0400

    Fix issue with command util when params contain spaces, and add unit test

--- src/main/java/mp44u/util/CommandUtility.java
+++ src/main/java/mp44u/util/CommandUtility.java
@@ -38,7 +38,7 @@ public class CommandUtility {
     public static String executeCommand(List<String> command, int maxTimeoutSeconds) {
         log.debug("Executing command with timeout {}s: {}", maxTimeoutSeconds, String.join(" ", command));
         CommandLine cmdLine = CommandLine.parse(command.getFirst());
-        cmdLine.addArguments(command.subList(1, command.size()).toArray(new String[0]));
+        command.subList(1, command.size()).forEach(arg -> cmdLine.addArgument(arg, false));
 
         DefaultExecutor executor = DefaultExecutor.builder().get();
         ExecuteWatchdog watchdog = null;
